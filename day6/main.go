af22a4b12c414f8150151b664feef521a2a101de
Jordan Radushev <dani.radushev@gmail.com>
2022-12-11 20:32:32 +0200

    Update main.go

--- day6/main.go
+++ day6/main.go
@@ -2,29 +2,20 @@ package day6
 
 import (
 	"bufio"
-	"fmt"
 )
 
 // TASK: https://adventofcode.com/2022/day/6
 
 func isValidMarker(chunk string) bool {
 	hashmap := map[int32]int{}
-
 	for _, item := range chunk {
-		fmt.Println(hashmap[item] == 0)
 		if hashmap[item] == 0 {
 			hashmap[item] = 1
 			continue
 		}
 		return false
 	}
-
 	return true
-}
-
-type InitialChunk struct {
-	Value           string
-	IndexOfLastChar int
 }
 
 func PartOne(scanner *bufio.Scanner) int {
@@ -34,34 +25,17 @@ func PartOne(scanner *bufio.Scanner) int {
 	}
 
 	t := ""
-	validMarker := InitialChunk{
-		Value:           "",
-		IndexOfLastChar: -1,
-	}
-	for i := 0; i < len(message); i++ {
-		fmt.Println(string(message[i]))
-		t += string(message[i])
-		fmt.Println("val: ", t)
-		// PART ONE
-		// if len(t) == 4 {
-		// 	if !isValidMarker(t) {
-		// 		t = t[1:]
-		// 	}
-		// 	validMarker.Value = t
-		// 	validMarker.IndexOfLastChar = i + 1
-		// }
 
-		// PART TWO
-		if len(t) == 14 {
+	idx := 0
+	for i := 0; i < len(message); i++ {
+		t += string(message[i])
+		// Change the condition t len(t) == 14 for part 2
+		if len(t) == 4 {
 			if !isValidMarker(t) {
 				t = t[1:]
 			}
-			validMarker.Value = t
-			validMarker.IndexOfLastChar = i + 1
+			idx = i + 1
 		}
-
 	}
-	fmt.Println(validMarker)
-
-	return -1
+	return idx
 }
